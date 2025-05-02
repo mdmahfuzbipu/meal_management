e764a43d1a9f4766414714b7b376658d0a81f7b2
mdmahfuzbipu <mdmahfuzbipu@gmail.com>
2025-05-02 21:39:24 +0600

    Added user login and logout using Django auth system

--- meal_management/meal_management/settings.py
+++ meal_management/meal_management/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+LOGIN_REDIRECT_URL = "/students/dashboard/"
+
+LOGIN_URL = "/login/"
+
 STATIC_URL = "static/"
 
 # Default primary key field type
--- meal_management/meal_management/urls.py
+++ meal_management/meal_management/urls.py
@@ -18,11 +18,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from core.views import home
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("managers/", include("managers.urls")),
-    path("", include("students.urls")),
+    path("students/", include("students.urls")),
+    
+    path("login/", auth_views.LoginView.as_view(template_name="students/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
     # path("students/", include("students.urls")),
     #path("", home, name="home"),
 ]
